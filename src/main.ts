e6a9d2ce940ded2e405e34efd0fa2e00ec27c77c
chenkui <848603414@qq.com>
2023-02-28 11:37:43 +0800

    0.1

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./routes/index";
+import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persist";
+const pinia = createPinia();
+pinia.use(piniaPersist);
+createApp(App).use(router).use(pinia).mount("#app");
